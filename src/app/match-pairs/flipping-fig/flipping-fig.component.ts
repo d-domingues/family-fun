efe9f64acd9f07109a2075f92e15220e5f0caffe
David Filipe Domingues <david.lopes.domingues@everis.com>
2020-05-01 22:41:41 +0100

    flipping wokimg

--- src/app/match-pairs/flipping-fig/flipping-fig.component.ts
+++ src/app/match-pairs/flipping-fig/flipping-fig.component.ts
@@ -1,67 +1,35 @@
-import { animate, keyframes, state, style, transition, trigger } from '@angular/animations'
+import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component, Input } from '@angular/core'
 import { Figure } from 'src/app/models/figure'
-
-import * as kf from './../../shared/keyframes'
 
 @Component({
 	selector: 'app-flipping-fig',
 	templateUrl: './flipping-fig.component.html',
 	styleUrls: ['./flipping-fig.component.scss'],
 	animations: [
-		trigger('flip', [
+		trigger('flipState', [
 			state(
-				'front',
-				style({
-					transform: 'rotateY(0deg)'
-				})
-			),
-			state(
-				'back',
+				'active',
 				style({
 					transform: 'rotateY(180deg)'
 				})
 			),
-			transition('front => back', [animate('1s 0s ease-out', keyframes(kf.flipBack))
-			]),
-			transition('back => front', [animate('1s 0s ease-in', keyframes(kf.flipFront))])
-		])
-		/* 		trigger('cardAnimator', [
-			transition('* => wobble', animate(1000, keyframes(kf.wobble))),
-			transition('* => swing', animate(1000, keyframes(kf.swing))),
-			transition('* => jello', animate(1000, keyframes(kf.jello))),
-			transition('* => zoomOutRight', animate(1000, keyframes(kf.zoomOutRight))),
-			transition('* => slideOutLeft', animate(1000, keyframes(kf.slideOutLeft))),
-			transition(
-				'* => rotateOutUpRight',
-				animate(1000, keyframes(kf.rotateOutUpRight))
+			state(
+				'inactive',
+				style({
+					transform: 'rotateY(0)'
+				})
 			),
-			transition('* => flipOutY', animate(1000, keyframes(kf.flipOutY)))
-		]) */
+			transition('active => inactive', animate('400ms ease-out')),
+			transition('inactive => active', animate('400ms ease-in'))
+		])
 	]
 })
 export class FlippingFigComponent {
 	@Input() figure: Figure
-	animationState: string
-	flipState = 'front'
+	flip = 'inactive'
 
-	startAnimation(stateAnim) {
-		console.log(stateAnim)
-		if (!this.animationState) {
-			this.animationState = stateAnim
-		}
-	}
-
-	resetAnimationState() {
-		this.animationState = ''
-	}
-
-
-	onFlipClick() {
-		if (this.flipState === 'front') {
-			this.flipState = 'back'
-		} else {
-			this.flipState = 'front'
-		}
+	toggleFlip() {
+		this.flip = this.flip === 'inactive' ? 'active' : 'inactive'
 	}
 }
